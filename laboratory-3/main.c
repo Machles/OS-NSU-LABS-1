54a2377be96ded8a15c9e0c894dac5e91d19ef62
machles <machlestv@gmail.com>
2021-02-17 22:11:55 +0700

    laboratory-3

---
+++ laboratory-3/main.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "stdio.h"
+#include <unistd.h>
+#include <sys/types.h>
+
+/*
+ * Создайте файл данных, который может писать и читать только владелец (это можно сделать командой shell chmod 600 file) и напишите программу, которая
+    Печатает реальный и эффективный идентификаторы пользователя.
+    Открывает файл с помощью fopen(3). Если fopen() завершился успешно, файл должен быть закрыт с помощью fclose(3).
+    Напечатайте сообщение об ошибке, используя perror(3C), если файл не удалось открыть.
+    Сделайте, чтобы реальный и эффективный идентификаторы пользователя совпадали. Подсказка: setuid(2)
+    Повторите первые два шага.
+
+Проверьте работу вашей программы.
+
+    Исполните программу и посмотрите вывод
+    Сделайте программу доступной для запуска членам вашей группы и пусть ваши одногруппники исполнят программу.
+    Командой chmod u+s prog установите бит установки идентификатора пользователя и пусть ваши одногруппники опять исполнят эту программу.
+ */
+
+int main(int argc, char* argv[]){
+    FILE* file = NULL;
+
+    file = fopen(argv[1], "r");
+
+    printf("Real user ID: %d\nEffective user ID: %d\n\n", getuid(), geteuid());
+
+    if(file == NULL){
+        perror("There are problems with opening file. (First try)");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("File is opened. (First try)");
+        fclose(file);
+    }
+
+    setuid(getuid());
+
+    file = fopen(argv[1], "r");
+
+    printf("New real user ID: %d\n New effective user ID: %d\n\n", getuid(), geteuid());
+
+    if(file == NULL){
+        perror("There are problems with opening file. (Second try)");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("File is opened. (Second try)");
+        fclose(file);
+    }
+
+    return EXIT_SUCCESS;
+}
