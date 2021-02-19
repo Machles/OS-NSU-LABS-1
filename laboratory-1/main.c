82c154f15eaf3d229fb6c14953350ef621876103
machles <machlestv@gmail.com>
2021-02-19 13:51:14 +0700

    laboratory-1: Done: -s, -v, -d, -p

--- laboratory-1/main.c
+++ laboratory-1/main.c
@@ -3,7 +3,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define PATH_MAX 1000
+#define PATH_MAX 100
+#define STATUS_SUCCESS 0
+#define STATUS_FAIL -1
 
 /*
  * Напишите программу, которая будет обрабатывать опции, приведенные ниже. Опции должны быть обработаны в соответствии с порядком своего появления справа налево.
@@ -11,15 +13,15 @@
  * Сначала пусть ваша программа обрабатывает только некоторые опции. Затем добавьте еще, до тех пор, пока все требуемые опции не будут обрабатываться.
  * Вы можете скопировать воспользоваться программой getopt_ex.c и изменить ее.
  *
- *  Done -i  Печатает реальные и эффективные идентификаторы пользователя и группы.
- *   -s  Процесс становится лидером группы. Подсказка: смотри setpgid(2).
- *   -p  Печатает идентификаторы процесса, процесса-родителя и группы процессов.
+ *  Done. -i  Печатает реальные и эффективные идентификаторы пользователя и группы.
+ *  Done. -s  Процесс становится лидером группы. Подсказка: смотри setpgid(2).
+ *  Done. -p  Печатает идентификаторы процесса, процесса-родителя и группы процессов.
  *   -u  Печатает значение ulimit
  *   -Unew_ulimit  Изменяет значение ulimit. Подсказка: смотри atol(3C) на странице руководства strtol(3C)
  *   -c  Печатает размер в байтах core-файла, который может быть создан.
  *   -Csize  Изменяет размер core-файла
- *   -d  Печатает текущую рабочую директорию
- *   -v  Распечатывает переменные среды и их значения
+ *  Done. -d  Печатает текущую рабочую директорию
+ *  Done. -v  Распечатывает переменные среды и их значения
  *   -Vname=value  Вносит новую переменную в среду или изменяет значение существующей переменной.
  *
  * Проверьте вашу программу на различных списках аргументов, в том числе:
@@ -30,21 +32,21 @@
  *   Неудачное значение для U.
  *
  */
+extern char ** environ;
 
 int main(int argc, char* argv[]){
     char options[] = "ispuU:cC:dvV:";
     int currentArg;
     currentArg = getopt(argc, argv, options);
 
-    char* pathHolder = (char*) malloc(PATH_MAX);
-    if(pathHolder == NULL){
-        perror("There are problems with allocating memory by malloc.");
-    }
+    char* pathHolder = NULL;
 
     if(argc == 1){
-        //fprintf(stderr, "")
+        printf( "No options here.\n");
         return 0;
     }
+
+    int status = STATUS_SUCCESS;
 
     while (currentArg !=  EOF){
         switch (currentArg) {
@@ -55,8 +57,17 @@ int main(int argc, char* argv[]){
                 printf("Effective group id: %d\n", getegid());
                 break;
             case 's':
+                status = setpgid(0, 0);
+                if(status == STATUS_SUCCESS){
+                    printf("Successfully process became leader of the group.");
+                } else {
+                    perror("There are problems with process's becoming leader of the group.");
+                }
                 break;
             case 'p':
+                printf("Process ID: %d\n", getpid());
+                printf("Parent Process ID: %d\n", getppid());
+                printf("Group of Processes ID: %d\n", getpgid(0) );
                 break;
             case 'u':
                 break;
@@ -67,7 +78,8 @@ int main(int argc, char* argv[]){
             case 'C':
                 break;
             case 'd':
-                if (getcwd(pathHolder, sizeof(pathHolder)) != NULL) {
+                pathHolder = getcwd(NULL, PATH_MAX);
+                if (pathHolder != NULL) {
                     printf("Current Working Directory: %s\n", pathHolder);
                 } else {
                     perror("There are problems with getcwd(). ");
@@ -75,9 +87,16 @@ int main(int argc, char* argv[]){
                 }
                 break;
             case 'v':
+                printf("Environmental Variables: \n");
+                for(char** envVariable = environ; *envVariable; ++envVariable){
+                    printf("%s\n", *envVariable);
+                }
                 break;
             case 'V':
                 break;
+            default:
+                fprintf(stderr, "Option isn't correct!");
+                exit(EXIT_FAILURE);
         }
         currentArg = getopt(argc, argv, options);
     }
