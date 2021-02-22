c8dc214cd7c9f3dd14c38e2075ee88c3ae6202e5
machles <machlestv@gmail.com>
2021-02-22 21:41:48 +0700

    laboratory-5 Almost done.

--- laboratory-5/main.c
+++ laboratory-5/main.c
@@ -7,13 +7,67 @@
 
 #define STATUS_SUCCESS 0
 #define STATUS_FAIL -1
+#define MAX_NUMBER_OF_STRING 1000
+
+void fillTable(long* offsetsFile_T, long* stringLengthFile_T, char* inputHolder, int fileDescriptorIn, int readingStatus){
+
+    size_t indexInInputHolder = 0;
+
+    size_t currentStringLength = 0;
+    size_t indexInTable = 0;
+
+    offsetsFile_T[0] = 0L;
+
+    while (readingStatus > 0){
+
+        while(indexInInputHolder < readingStatus){
+
+            if(inputHolder[indexInInputHolder] == '\n'){
+                currentStringLength++;
+                offsetsFile_T[indexInTable] = currentStringLength;
+                stringLengthFile_T[indexInTable++] = lseek(fileDescriptorIn, 0L, SEEK_CUR);
+                indexInInputHolder = 0;
+            } else {
+                currentStringLength++;
+                indexInInputHolder++;
+            }
+
+        }
+        if( (readingStatus = read(fileDescriptorIn, inputHolder, INPUT_HOLDER_SIZE)) == STATUS_FAIL){
+            perror("There are problems with reading file.");
+        }
+    }
+
+}
+
+char* getStringByNumber(int fileDescriptorIn, long* offsetFile_T, long* stringLengthFile_T, char* inputHolder){
+    int stringNumber = -1;
+    size_t indexInInputHolder = 0;
+    int currentBufferSize = INPUT_HOLDER_SIZE;
+
+    while(printf("Enter number of line, which you want to see: ") && scanf("%d", &stringNumber)){
+        if(stringNumber < 0 || stringNumber >= MAX_NUMBER_OF_STRING){
+            fprintf(stderr, "Invalid string number!");
+        }
+
+        if(stringNumber == 0){
+            printf("Stop number!");
+            exit(EXIT_FAILURE);
+        }
+
+        lseek(fileDescriptorIn, offsetFile_T[stringNumber+1], SEEK_CUR);
+
+        if( stringLengthFile_T[stringNumber+1] < currentBufferSize){
+            currentBufferSize = stringLengthFile_T[stringNumber+1];
+            realloc(inputHolder, currentBufferSize);
+        }
+    }
+}
 
 int main(int argc, char* argv[]){
 
     int fileDescriptorIn = 0;
-    size_t indexInInputHolder = 0;
-    size_t currentStringLegth = 0;
-    size_t indexInTable = 0;
+    int readingStatus;
 
     long offsetsFile_T[256];
     long stringLengthFile_T[256];
@@ -24,36 +78,21 @@ int main(int argc, char* argv[]){
         exit(EXIT_FAILURE);
     }
 
-    int stringNumber = -1;
-
-    printf("Enter number of line, which you want to see: ");
-
-    stringNumber = atoi(argv[2]);
-
     if(argc < 3){
         printf("Not enough arguments entered.");
     }
 
-    fileDescriptorIn = open(argv[1], O_RDONLY, 0600);
-    if(fileDescriptorIn == STATUS_FAIL){
+    if( (fileDescriptorIn = open(argv[1], O_RDONLY, 0600)) == STATUS_FAIL){
         perror("There are problems while reading file.");
     }
 
-    int readingStatus = read(fileDescriptorIn, inputHolder, INPUT_HOLDER_SIZE);
-
-    if(readingStatus == STATUS_FAIL){
+    if( (readingStatus = read(fileDescriptorIn, inputHolder, INPUT_HOLDER_SIZE)) == STATUS_FAIL){
         perror("There are problems with reading file.");
     }
 
-    while (readingStatus > 0){
-        if(inputHolder[indexInInputHolder] == '\n'){
-            currentStringLegth++;
-            offsetsFile_T[indexInTable] = currentStringLegth;
-            stringLengthFile_T[indexInTable++] = lseek(fileDescriptorIn, 0L, SEEK_CUR);
-            indexInInputHolder = 0;
-        } else {
-            currentStringLegth++;
-        }
-        readingStatus = read(fileDescriptorIn, inputHolder, INPUT_HOLDER_SIZE);
-    }
+    fillTable(offsetsFile_T, stringLengthFile_T, inputHolder, fileDescriptorIn, readingStatus);
+
+    getStringByNumber(inputHolder);
+
+    return EXIT_SUCCESS;
 }
