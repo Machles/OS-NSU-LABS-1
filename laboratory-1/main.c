c8df86593363737e5497571ede97560eb75bfde0
machles <machlestv@gmail.com>
2021-03-02 21:13:27 +0700

    laboratory-1 refactoring main

--- laboratory-1/main.c
+++ laboratory-1/main.c
@@ -9,49 +9,106 @@
 #define STATUS_SUCCESS 0
 #define STATUS_FAIL -1
 
-/*
- *
- * Напишите программу, которая будет обрабатывать опции, приведенные ниже. Опции должны быть обработаны в соответствии с порядком своего появления справа налево.
- * Одной и той же опции разрешено появляться несколько раз. Используйте getopt(3C) для определения имеющихся опций.
- * Сначала пусть ваша программа обрабатывает только некоторые опции. Затем добавьте еще, до тех пор, пока все требуемые опции не будут обрабатываться.
- * Вы можете скопировать воспользоваться программой getopt_ex.c и изменить ее.
- *
- *  Done. -i  Печатает реальные и эффективные идентификаторы пользователя и группы.
- *  Done. -s  Процесс становится лидером группы. Подсказка: смотри setpgid(2).
- *  Done. -p  Печатает идентификаторы процесса, процесса-родителя и группы процессов.
- *  Done. -u  Печатает значение ulimit
- *  Done. -Unew_ulimit  Изменяет значение ulimit. Подсказка: смотри atol(3C) на странице руководства strtol(3C)
- *  Done. -c  Печатает размер в байтах core-файла, который может быть создан.
- *  Done. -Csize  Изменяет размер core-файла
- *  Done. -d  Печатает текущую рабочую директорию
- *  Done. -v  Распечатывает переменные среды и их значения
- *  Done. -Vname=value  Вносит новую переменную в среду или изменяет значение существующей переменной.
- *
- * Проверьте вашу программу на различных списках аргументов, в том числе:
- *
- *  Done. Нет аргументов
- *  Done. Недопустимую опцию.
- *   Опции, разделенные знаком минус.
- *   Неудачное значение для U.
- *
- */
 extern char ** environ;
+
+void printRealAndEffectiveUIDAndGID(){
+    printf("Real user id: %d\n", getuid());
+    printf("Effective user id: %d\n", geteuid());
+    printf("Real group id: %d\n", getgid());
+    printf("Effective group id: %d\n", getegid());
+}
+
+void makeProcessGroupLeader(){
+    int status = setpgid(0, 0);
+    if(status == STATUS_SUCCESS){
+        printf("Successfully process became leader of the group.");
+    } else {
+        perror("There are problems with process's becoming leader of the group");
+    }
+}
+
+void printPIDandPPIDandPGID(){
+    printf("Process ID: %d\n", getpid());
+    printf("Parent Process ID: %d\n", getppid());
+    printf("Group of Processes ID: %d\n", getpgid(0) );
+}
+
+void printUlimitValue(){
+    long ulimitValue = STATUS_FAIL;
+
+    ulimitValue = ulimit(UL_GETFSIZE);
+    if(ulimitValue == STATUS_FAIL){
+        perror("There are problems with getting ulimit.\n");
+    } else {
+        printf("Current max file size for process is: %lu\n", ulimitValue);
+    }
+}
+
+void changeUlimitValue(){
+    char* UValuePointer = optarg;
+    char* endptr;
+
+    long stringNumber = strtol(optarg, &endptr, 10);
+    if(stringNumber == 0 && endptr != NULL || stringNumber < 0){
+        fprintf(stderr, "Invalid ulimit value");
+    }
+
+    int status = ulimit(UL_SETFSIZE, atol(optarg));
+    if(status == STATUS_FAIL){
+        perror("There are problems with setting new ulimit value.\n");
+    }
+
+    printf("%s", UValuePointer);
+}
+
+void printCoreFileSize(){
+    struct rlimit rlp;
+    int coreFileSize = getrlimit(RLIMIT_CORE, &rlp);
+    if(coreFileSize == STATUS_FAIL){
+        perror("There are problems with getting core-file size.");
+    }
+    printf("Core Size is: %lu", rlp.rlim_cur );
+}
+
+void changeCoreFileSize(){
+    struct rlimit rlp;
+    char* CValuePointer = optarg;
+
+    getrlimit(RLIMIT_CORE, &rlp);
+    rlp.rlim_cur = atol(CValuePointer);
+
+    if (setrlimit(RLIMIT_CORE, &rlp) == STATUS_FAIL)
+        perror( "There are some problems with setting new value of ulimit.\n");
+}
+
+void printCurrentDirectory(){
+    char* pathHolder = getcwd(NULL, PATH_MAX);
+    if (pathHolder != NULL) {
+        printf("Current Working Directory: %s\n", pathHolder);
+    } else {
+        perror("There are problems with getcwd().");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void printEnvironmentVariables(){
+    printf("Environmental Variables: \n");
+    for(char** envVariable = environ; *envVariable; ++envVariable){
+        printf("%s\n", *envVariable);
+    }
+}
+
+void changeOrAddEnvironmentVariable(){
+    char* VValuePointer = optarg;
+    int status = putenv(VValuePointer);
+    if(status != STATUS_SUCCESS){
+        perror("There are problems with putting new environmental variable.");
+    }
+}
 
 int main(int argc, char* argv[]){
     char options[] = "ispuU:cC:dvV:";
-    int currentArg;
-    currentArg = getopt(argc, argv, options);
-
-    struct rlimit rlp;
-    int coreFileSize = STATUS_FAIL;
-    long ulimitValue = STATUS_FAIL;
-    long status = STATUS_SUCCESS;
-
-    char* UValuePointer;
-    char* CValuePointer;
-    char* VValuePointer;
-
-    char* pathHolder = NULL;
+    int currentArg = getopt(argc, argv, options);
 
     if(argc == 1){
         printf( "No options here.\n");
@@ -61,78 +118,25 @@ int main(int argc, char* argv[]){
     while (currentArg !=  EOF){
         switch (currentArg) {
             case 'i':
-                printf("Real user id: %d\n", getuid());
-                printf("Effective user id: %d\n", geteuid());
-                printf("Real group id: %d\n", getgid());
-                printf("Effective group id: %d\n", getegid());
-                break;
+                printRealAndEffectiveUIDAndGID(); break;
             case 's':
-                status = setpgid(0, 0);
-                if(status == STATUS_SUCCESS){
-                    printf("Successfully process became leader of the group.");
-                } else {
-                    perror("There are problems with process's becoming leader of the group.");
-                }
-                break;
+                makeProcessGroupLeader(); break;
             case 'p':
-                printf("Process ID: %d\n", getpid());
-                printf("Parent Process ID: %d\n", getppid());
-                printf("Group of Processes ID: %d\n", getpgid(0) );
-                break;
+                printPIDandPPIDandPGID(); break;
             case 'u':
-                ulimitValue = ulimit(UL_GETFSIZE);
-                if(ulimitValue == STATUS_FAIL){
-                    perror("There are problems with getting ulimit.\n");
-                } else {
-                    printf("Current max file size for process is: %lu\n", ulimitValue);
-                }
-                break;
+                printUlimitValue(); break;
             case 'U':
-                UValuePointer = optarg;
-                status = ulimit(UL_SETFSIZE, atol(optarg));
-                if(status == STATUS_FAIL){
-                    perror("There are problems with setting new ulimit value.\n");
-                }
-                printf("%s", UValuePointer);
-                break;
+                changeUlimitValue(); break;
             case 'c':
-                coreFileSize = getrlimit(RLIMIT_CORE, &rlp);
-                if(coreFileSize == STATUS_FAIL){
-                    perror("There are problems with getting core-file size.");
-                }
-                printf("Core Size is: %lu", rlp.rlim_cur );
-                break;
+                printCoreFileSize(); break;
             case 'C':
-                CValuePointer = optarg;
-
-                getrlimit(RLIMIT_CORE, &rlp);
-                rlp.rlim_cur = atol(CValuePointer);
-
-                if (setrlimit(RLIMIT_CORE, &rlp) == STATUS_FAIL)
-                    perror( "There are some problems with setting new value of ulimit.\n");
-                break;
+                changeCoreFileSize(); break;
             case 'd':
-                pathHolder = getcwd(NULL, PATH_MAX);
-                if (pathHolder != NULL) {
-                    printf("Current Working Directory: %s\n", pathHolder);
-                } else {
-                    perror("There are problems with getcwd().");
-                    exit(EXIT_FAILURE);
-                }
-                break;
+                printCurrentDirectory(); break;
             case 'v':
-                printf("Environmental Variables: \n");
-                for(char** envVariable = environ; *envVariable; ++envVariable){
-                    printf("%s\n", *envVariable);
-                }
-                break;
+                printEnvironmentVariables(); break;
             case 'V':
-                VValuePointer = optarg;
-                status = putenv(VValuePointer);
-                if(status != STATUS_SUCCESS){
-                    perror("There are problems with putting new environmental variable.");
-                }
-                break;
+                changeOrAddEnvironmentVariable(); break;
             case '?':
                 fprintf(stderr, "Option isn't correct!\n");
                 exit(EXIT_FAILURE);
