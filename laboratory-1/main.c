94503e379ca09d072d603cb62f7fbe3675c2e4f5
machles <machlestv@gmail.com>
2021-03-03 22:17:20 +0700

    laboratory-1

--- laboratory-1/main.c
+++ laboratory-1/main.c
@@ -11,40 +11,48 @@
 
 extern char ** environ;
 
-void printRealAndEffectiveUIDAndGID(){
+int printRealAndEffectiveUIDAndGID(){
     printf("Real user id: %d\n", getuid());
     printf("Effective user id: %d\n", geteuid());
     printf("Real group id: %d\n", getgid());
     printf("Effective group id: %d\n", getegid());
+
+    return STATUS_SUCCESS;
 }
 
-void makeProcessGroupLeader(){
+int makeProcessGroupLeader(){
     int status = setpgid(0, 0);
-    if(status == STATUS_SUCCESS){
-        printf("Successfully process became leader of the group.");
-    } else {
+    if(status != STATUS_SUCCESS){
         perror("There are problems with process's becoming leader of the group");
+        return STATUS_FAIL;
     }
+    printf("Successfully process became leader of the group.");
+
+    return STATUS_SUCCESS;
 }
 
-void printPIDandPPIDandPGID(){
+int printPIDandPPIDandPGID(){
     printf("Process ID: %d\n", getpid());
     printf("Parent Process ID: %d\n", getppid());
     printf("Group of Processes ID: %d\n", getpgid(0) );
+
+    return STATUS_SUCCESS;
 }
 
-void printUlimitValue(){
-    long ulimitValue = STATUS_FAIL;
+int printUlimitValue(){
+    long ulimitValue = ulimit(UL_GETFSIZE);
 
-    ulimitValue = ulimit(UL_GETFSIZE);
     if(ulimitValue == STATUS_FAIL){
-        perror("There are problems with getting ulimit.\n");
-    } else {
-        printf("Current max file size for process is: %lu\n", ulimitValue);
+        perror("There are problems with getting ulimit");
+        return STATUS_FAIL;
     }
+
+    printf("Current max file size for process is: %lu\n", ulimitValue);
+
+    return STATUS_SUCCESS;
 }
 
-void changeUlimitValue(){
+int changeUlimitValue(){
     char* UValuePointer = optarg;
     char* endptr;
 
@@ -55,20 +63,25 @@ void changeUlimitValue(){
 
     long status = ulimit(UL_SETFSIZE, stringNumber);
     if(status == STATUS_FAIL){
-        perror("There are problems with setting new ulimit value.\n");
+        perror("There are problems with setting new ulimit value");
+        return STATUS_FAIL;
     }
+    return STATUS_SUCCESS;
 }
 
-void printCoreFileSize(){
+int printCoreFileSize(){
     struct rlimit rlp;
     int coreFileSize = getrlimit(RLIMIT_CORE, &rlp);
     if(coreFileSize == STATUS_FAIL){
-        perror("There are problems with getting core-file size.");
+        perror("There are problems with getting core-file size");
+        return STATUS_FAIL;
     }
     printf("Core Size is: %lu", rlp.rlim_cur );
+
+    return STATUS_SUCCESS;
 }
 
-void changeCoreFileSize(){
+int changeCoreFileSize(){
     struct rlimit rlp;
     char* CValuePointer = optarg;
     char* endptr;
@@ -77,43 +90,55 @@ void changeCoreFileSize(){
 
     long stringNumber = strtol(CValuePointer, &endptr, 10);
     if(stringNumber == 0 && endptr != NULL || stringNumber < 0){
-        fprintf(stderr, "Invalid core file size");
+        fprintf(stderr, "Invalid core-file size");
     }
 
     rlp.rlim_cur = stringNumber;
 
-    if (setrlimit(RLIMIT_CORE, &rlp) == STATUS_FAIL)
-        perror( "There are some problems with setting new value of ulimit.\n");
-}
-
-void printCurrentDirectory(){
-    char* pathHolder = getcwd(NULL, PATH_MAX);
-    if (pathHolder != NULL) {
-        printf("Current Working Directory: %s\n", pathHolder);
-    } else {
-        perror("There are problems with getcwd().");
-        exit(EXIT_FAILURE);
+    if (setrlimit(RLIMIT_CORE, &rlp) == STATUS_FAIL) {
+        perror("There are some problems with setting new value of ulimit");
+        return STATUS_FAIL;
     }
+
+    return STATUS_SUCCESS;
 }
 
-void printEnvironmentVariables(){
+int printCurrentDirectory(){
+    char* pathHolder = getcwd(NULL, PATH_MAX);
+    if (pathHolder == NULL) {
+        perror("There are problems with getcwd().");
+        return STATUS_FAIL;
+    }
+
+    printf("Current Working Directory: %s\n", pathHolder);
+
+    return STATUS_SUCCESS;
+}
+
+int printEnvironmentVariables(){
     printf("Environmental Variables: \n");
     for(char** envVariable = environ; *envVariable; ++envVariable){
         printf("%s\n", *envVariable);
     }
+
+    return STATUS_SUCCESS;
 }
 
-void changeOrAddEnvironmentVariable(){
+int changeOrAddEnvironmentVariable(){
     char* VValuePointer = optarg;
     int status = putenv(VValuePointer);
     if(status != STATUS_SUCCESS){
-        perror("There are problems with putting new environmental variable.");
+        perror("There are problems with putting new environmental variable");
+        return STATUS_FAIL;
     }
+
+    return STATUS_SUCCESS;
 }
 
 int main(int argc, char* argv[]){
     char options[] = "ispuU:cC:dvV:";
     int currentArg = getopt(argc, argv, options);
+    int status = STATUS_FAIL;
 
     if(argc == 1){
         printf( "No options here.\n");
@@ -123,25 +148,54 @@ int main(int argc, char* argv[]){
     while (currentArg !=  EOF){
         switch (currentArg) {
             case 'i':
-                printRealAndEffectiveUIDAndGID(); break;
+                printRealAndEffectiveUIDAndGID();
+                break;
             case 's':
-                makeProcessGroupLeader(); break;
+                status = makeProcessGroupLeader();
+                if(status == STATUS_FAIL){
+                    exit(EXIT_FAILURE);
+                }
+                break;
             case 'p':
                 printPIDandPPIDandPGID(); break;
             case 'u':
-                printUlimitValue(); break;
+                status = printUlimitValue();
+                if(status == STATUS_FAIL){
+                    exit(EXIT_FAILURE);
+                }
+                break;
             case 'U':
-                changeUlimitValue(); break;
+                status = changeUlimitValue();
+                if(status == STATUS_FAIL){
+                    exit(EXIT_FAILURE);
+                }
+                break;
             case 'c':
-                printCoreFileSize(); break;
+                status = printCoreFileSize();
+                if(status == STATUS_FAIL){
+                    exit(EXIT_FAILURE);
+                }
+                break;
             case 'C':
-                changeCoreFileSize(); break;
+                status = changeCoreFileSize();
+                if(status == STATUS_FAIL){
+                    exit(EXIT_FAILURE);
+                }
+                break;
             case 'd':
-                printCurrentDirectory(); break;
+                status = printCurrentDirectory();
+                if(status == STATUS_FAIL){
+                    exit(EXIT_FAILURE);
+                }
+                break;
             case 'v':
                 printEnvironmentVariables(); break;
             case 'V':
-                changeOrAddEnvironmentVariable(); break;
+                status = changeOrAddEnvironmentVariable();
+                if(status == STATUS_FAIL){
+                    exit(EXIT_FAILURE);
+                }
+                break;
             case '?':
                 fprintf(stderr, "Option isn't correct!\n");
                 exit(EXIT_FAILURE);
