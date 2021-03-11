9758ab75b8dcf2947bdcf4077a830e4e980fa513
machles <machlestv@gmail.com>
2021-03-11 13:00:35 +0700

    laboratory-5 with comments and answers

--- laboratory-5/main.c
+++ laboratory-5/main.c
@@ -13,12 +13,12 @@
 #define STDIN 0
 
 extern int errno;
+
 /// Объясните почему нельзя использовать scanf?
 /* strtol будет лучше и понятнее себя вести, когда на вход попадет строка, содержащая очень больше число,
  * если число окажется больше LONG_MAX или меньше LONG_MIN - strtol вернёт -1 и установит errno ERANGE.
  * В свою очередь, scanf даже не уведомит о том, что что-то не так
  */
-
 long getStringNumber(int stringsCount){
     char *endptr = NULL;
     printf("There are %d strings.\nEnter number of line, which you want to see: ", stringsCount);
@@ -94,7 +94,8 @@ int fillTable(long* offsetsFileTable, long* stringsLengthsFileTable, int fileDes
     return indexInTable;
 }
 
-// На самом деле ваша функция ничего не возвращает. Название неподходящее. - Поменял
+/// На самом деле ваша функция ничего не возвращает. Название неподходящее.
+/// Поменял
 int printStringByNumber(int fileDescriptorIn, long* offsetFileTable, const long* stringsLengthsFileTable, int stringsCount){
     long currentBufferSize = INPUT_HOLDER_SIZE;
     long stopNumber = 0;
@@ -106,9 +107,11 @@ int printStringByNumber(int fileDescriptorIn, long* offsetFileTable, const long*
     if(stringNumber == -1){
         return STATUS_FAIL;
     }
-    // избегайте слова "flag" в переменных, в большинстве случаев это бессмысленный суффикс
-    // подумайте, каков общий смысл, до каких пор выполняется обработка строк? соответственно дайте название предикату
-    // неявное приведение типов, используйте сравнение с конкретным значением - Флаг заменил на условие достиженяи стопсимвола.
+
+    /// избегайте слова "flag" в переменных, в большинстве случаев это бессмысленный суффикс
+    /// подумайте, каков общий смысл, до каких пор выполняется обработка строк? соответственно дайте название предикату
+    /// неявное приведение типов, используйте сравнение с конкретным значением
+    /// Флаг заменил на условие достижения стопсимвола.
     while(stringNumber != stopNumber){
 
         if(stringNumber < 0 || stringNumber > stringsCount){
@@ -125,12 +128,13 @@ int printStringByNumber(int fileDescriptorIn, long* offsetFileTable, const long*
             continue;
         }
 
-        /// В описании вы указали, что lseek может возвращать ошибки (какие?) - но обработчика ошибок нет - теперь есть, ниже указаны ошибки
+        /// В описании вы указали, что lseek может возвращать ошибки (какие?) - но обработчика ошибок нет
+        /// Теперь есть, ниже указаны ошибки.
         status = lseek(fileDescriptorIn, offsetFileTable[stringNumber-1], SEEK_SET);
         if(status == STATUS_FAIL){
             perror("There are problems while printing string by number, exactly with setting position in file");
-            // Какие ошибки может вернуть lseek?
-            // EBADF, ESPIPE, EINVAL, EOVERFLOW, ENXIO - Ошибки lseek
+            /// Какие ошибки может вернуть lseek?
+            /// EBADF, ESPIPE, EINVAL, EOVERFLOW, ENXIO - Ошибки lseek
             return STATUS_FAIL;
         }
 
@@ -139,6 +143,10 @@ int printStringByNumber(int fileDescriptorIn, long* offsetFileTable, const long*
         if(stringHolder == NULL){
             perror("There are problems while printing string by number, exactly with allocating memory with malloc");
             free(stringHolder);
+
+            /// exit внутри функций - это, как правило, неожиданное поведение и очень плохая практика
+            /// возвращайте код ошибки, и обрабатывайте его "сверху"
+            /// Исправил
             return STATUS_FAIL;
         }
 
@@ -147,6 +155,9 @@ int printStringByNumber(int fileDescriptorIn, long* offsetFileTable, const long*
         if(readSymbols == STATUS_FAIL){
             perror("String number is invalid");
             free(stringHolder);
+            ///Это очень плохая практика в любой обработке ошибки завершать процесс - это неожиданное поведение, такой код практически нельзя переиспользовать, он очень нестабилен. Нужно исправить.
+            /// В общем случае при ошибке: освобождайте ресурсы (где требуется), возвращайте код ошибки, и далее в основном коде обрабатывайте ошибку
+            /// Исправил
             return STATUS_FAIL;
         }
 
@@ -165,20 +176,23 @@ int main(int argc, char* argv[]){
 
     int fileDescriptorIn = 0;
 
-    /// Названия переменных плохие. Вы одновременно используете camelCase и "_" - выбере что-то одно. "_T" - не говорит ни чём - исправил
+    /// Названия переменных плохие. Вы одновременно используете camelCase и "_" - выбере что-то одно. "_T" - не говорит ни чём
+    /// Исправил
     long offsetsFileTable[256];
     long stringsLengthsFileTable[256];
     int status;
 
     if(argc < 2){
         /// Куда происходит вывод сообщение об ошибке сейчас? а куда следует на самом деле выводить сообщение об ошибке?
+        /// Вывод сообщения об ошибке сейчас происходит в стандартный поток вывода, а следует выводить в поток вывода ошибок stderr
         /// Опишите пример usage для пользотеля так, как это делается в стандартных утилитах. Иначе как пользователь угадает, что от него хотят?
-//      /// Описал
+        /// Описал
         fprintf(stderr, "Not enough arguments entered.\nusage: progname input_file\n");
         exit(EXIT_FAILURE);
     }
 
-    /// Что такое "0600"? Замените на именованную константу или макрос - Это 8ричное число, которое указывает, что файл читать и писать может только владелец.
+    /// Что такое "0600"? Замените на именованную константу или макрос
+    /// Это 8ричное число, которое указывает, что файл читать и писать может только владелец. Заменил на макрос.
     if( (fileDescriptorIn = open(argv[1], O_RDONLY, OWNER_READ_WRITE)) == STATUS_FAIL){
         perror("There are problems while reading file.");
         exit(EXIT_FAILURE);
@@ -187,6 +201,8 @@ int main(int argc, char* argv[]){
     int stringsCount = fillTable(offsetsFileTable, stringsLengthsFileTable, fileDescriptorIn);
 
     if(stringsCount == STATUS_FAIL){
+        /// Количество элементов принято обозначать словом "count". Amount - это скорее "сумма", про валюту
+        /// Исправил
         return EXIT_FAILURE;
     }
 
