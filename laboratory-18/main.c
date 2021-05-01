39df0bfafb7405774043a4eff12ea6c6ac3dc8dc
machles <machlestv@gmail.com>
2021-05-01 18:17:02 +0700

    lab-18 printFileType printFilePermissions

---
+++ laboratory-18/main.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+#define MIN_REQUIRED_ARGS_NUM 2
+#define FLAGS_COUNT 3
+#define PERMISSION_BITS_COUNT 9
+#define TRUE 1
+#define STAT_ERROR 1
+#define NOT_INCLUDED 0
+
+void printFilePermissions(struct stat* statbuf){
+    mode_t mode = statbuf->st_mode;
+
+    const char flags[FLAGS_COUNT] = {'r', 'w', 'x'};
+    char permissions[PERMISSION_BITS_COUNT] = "---------";
+    const mode_t permissionBits[PERMISSION_BITS_COUNT] = { S_IRUSR, S_IWUSR, S_IXUSR,
+                                                           S_IRGRP, S_IWGRP, S_IXGRP,
+                                                           S_IROTH, S_IWOTH, S_IXOTH };
+
+    for (int i = 0; i < PERMISSION_BITS_COUNT; ++i) {
+        if( (mode & permissionBits[i]) != NOT_INCLUDED){
+            permissions[i] = flags[i % FLAGS_COUNT];
+        }
+    }
+
+    printf("%s ", permissions);
+}
+
+void printFileType(struct stat* statbuf){
+    mode_t mode = statbuf->st_mode;
+    if(S_ISDIR(mode)){
+        printf("%c", 'd');
+    } if(S_ISREG(mode)){
+        printf("%c", '-');
+    } else {
+        printf("%c", '?');
+    }
+}
+
+void printFileInformation(struct stat* statbuf){
+    printFileType(statbuf);
+    printFilePermissions(statbuf);
+}
+
+int main(int argc, char **argv){
+
+    struct stat statbuf;
+    char * pathFile;
+    int statStatus;
+
+    if(argc < MIN_REQUIRED_ARGS_NUM){
+        fprintf(stderr, "Not enough arguments entered.\nusage: progname <file1> <file2> ... <fileN>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        pathFile = argv[i];
+        statStatus = stat(pathFile, &statbuf);
+        if(statStatus == STAT_ERROR){
+            perror("There are problems with stat.");
+            continue;
+        }
+        printFileInformation(&statbuf);
+    }
+
+    return EXIT_SUCCESS;
+}
+
---
+++ laboratory-19/main.c
@@ -0,0 +1,4 @@
+//
+// Created by yattroman on 01.05.2021.
+//
+
